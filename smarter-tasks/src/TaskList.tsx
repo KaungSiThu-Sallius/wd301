6513bd2db153387ce0f398abedcb04639f561b0c
Kaung Si Thu <admin@Kaungs-MacBook-Pro.local>
2023-08-23 12:21:58 +0530

    level 4 hooks

--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Task from "./Task";
 import { TaskItem } from "./types.ts";
 
@@ -7,15 +6,16 @@ interface Props {
 }
 
 
-interface State {
-
-}
-
-class TaskList extends React.Component<Props, State>{
-
-    render() {
-        return this.props.tasks.map((task, idx) => <Task key={idx} title={task.title} description={task.description} dueDate={task.dueDate} />);
-    }
-}
+const TaskList = (props: Props) => {
+    const list = props.tasks.map((task, idx) => (
+        <Task
+            key={idx}
+            title={task.title}
+            description={task.description}
+            dueDate={task.dueDate}
+        />
+    ));
+    return <>{list}</>;
+};
 
 export default TaskList
