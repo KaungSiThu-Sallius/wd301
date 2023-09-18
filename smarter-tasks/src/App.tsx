e51eed6f0c1c6334c12854ace821a82cd30f8003
Kaung Si Thu <admin@Kaungs-MacBook-Pro.local>
2023-09-18 18:15:35 +0530

    Challeng6 01

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,61 +1,48 @@
 import {
   createBrowserRouter,
-  Navigate,
   RouterProvider,
 } from "react-router-dom";
-
-import HomePage from "./pages/HomePage";
-import TaskListPage from "./pages/TaskListPage";
-import TaskDetailsPage from "./pages/TaskDetailsPage";
-import Signin from "./pages/Signin";
+import Notfound from "./pages/Notfound";
+import Signup from './pages/signup';
+import Signin from './pages/signin/SigninForm';
+import Dashboard from "./pages/dashboard";
 import ProtectedRoute from "./ProtectedRoute";
-import Layout from "./Layout";
-import NotFound from "./pages/Notfound";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/signin" replace />,
+    element: <Signup />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
   {
     path: "/signin",
     element: <Signin />,
   },
   {
-    path: '*',
-    element: <Navigate to="/notfound" replace />,
+    path: "/notfound",
+    element: <Notfound />,
   },
   {
-    path: '/notfound',
-    element: <NotFound />
-  },
-  {
+    path: "/dashboard",
     element: (
       <ProtectedRoute>
-        <Layout />
+        <Dashboard />
       </ProtectedRoute>
     ),
-    children: [
-      {
-        path: "home",
-        element: <HomePage />,
-      },
-      {
-        path: "tasks",
-        element: <TaskListPage />,
-      },
-      {
-        path: "tasks/:id",
-        element: <TaskDetailsPage />,
-      },
-
-    ],
   },
-
+  {
+    path: "*",
+    element: <Notfound />,
+  }
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
-};
+  return (
+    <RouterProvider router={router} />
+  );
+}
 
-export default App;
+export default App
