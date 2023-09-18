d97424e67da5adfccb93fb3b6a8aef4450705f05
Kaung Si Thu <admin@Kaungs-MacBook-Pro.local>
2023-09-18 19:01:34 +0530

    Challeng6 03

--- smarter-tasks/src/pages/dashboard/index.tsx
+++ smarter-tasks/src/pages/dashboard/index.tsx
@@ -14,6 +14,7 @@ const Dashboard: React.FC = () => {
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <a href="/signin" id='logout-link'>Sign Out</a>
             <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Dashboard</h1>
             <p>Name: {userName}</p>
             <p>Email: {email}</p>
