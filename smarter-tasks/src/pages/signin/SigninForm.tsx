90c59b88e28825f58a127afae92b5d5b02dfc31d
Kaung Si Thu <admin@Kaungs-MacBook-Pro.local>
2023-09-26 23:08:47 +0530

    week7 challenge02

--- smarter-tasks/src/pages/signin/SigninForm.tsx
+++ smarter-tasks/src/pages/signin/SigninForm.tsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 // First we will import the API_ENDPOINT constant from the `config` folder
 import { API_ENDPOINT } from '../../config/constants';
 import { useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form"
 
+type Inputs = {
+    email: string
+    password: string
+}
 const SigninForm: React.FC = () => {
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const navigate = useNavigate();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>()
+
     localStorage.setItem('authToken', "");
     localStorage.setItem('userData', "");
 
     // Then we will define the handle submit function
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        const { email, password } = data
         try {
             const response = await fetch(`${API_ENDPOINT}/users/sign_in`, {
                 method: 'POST',
@@ -28,12 +38,12 @@ const SigninForm: React.FC = () => {
             console.log('Sign-in successful');
 
             // extract the response body as JSON data
-            const data = await response.json();
+            const _data = await response.json();
 
 
             // After successful signin, first we will save the token in localStorage
-            localStorage.setItem('authToken', data.token);
-            localStorage.setItem('userData', JSON.stringify(data.user));
+            localStorage.setItem('authToken', _data.token);
+            localStorage.setItem('userData', JSON.stringify(_data.user));
             navigate("/account");
 
 
@@ -43,14 +53,16 @@ const SigninForm: React.FC = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <label className="block text-gray-700 font-semibold mb-2">Email:</label>
-                <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                <input {...register("email", { required: true })} type="email" name="email" id="email" className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                {errors.email && <span>This field is required</span>}
             </div>
             <div>
                 <label className="block text-gray-700 font-semibold mb-2">Password:</label>
-                <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                <input {...register("password", { required: true })} type="password" name="password" id="password" className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                {errors.password && <span>This field is required</span>}
             </div>
             <button type="submit" className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4">Sign In</button>
         </form>
--- smarter-tasks/src/pages/signup/SignupForm.tsx
+++ smarter-tasks/src/pages/signup/SignupForm.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { API_ENDPOINT } from '../../config/constants';
 import { useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form"
+
+type Inputs = {
+    organisationName: string
+    userName: string
+    userEmail: string
+    userPassword: string
+}
 const SignupForm: React.FC = () => {
-    const [organisationName, setOrganisationName] = useState('');
-    const [userName, setUserName] = useState('');
-    const [userEmail, setUserEmail] = useState('');
-    const [userPassword, setUserPassword] = useState('');
     localStorage.setItem('authToken', "");
     localStorage.setItem('userData', "");
     const navigate = useNavigate();
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>()
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        const { organisationName, userName, userEmail, userPassword } = data
 
         try {
             const response = await fetch(`${API_ENDPOINT}/organisations`, {
@@ -37,22 +46,26 @@ const SignupForm: React.FC = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <label className="block text-gray-700 font-semibold mb-2">Organisation Name:</label>
-                <input type="text" name="organisationName" id="organisationName" value={organisationName} onChange={(e) => setOrganisationName(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                <input  {...register("organisationName", { required: true })} type="text" name="organisationName" id="organisationName" className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                {errors.organisationName && <span>This field is required</span>}
             </div>
             <div>
                 <label className="block text-gray-700 font-semibold mb-2">Your Name:</label>
-                <input type="text" name="userName" id="userName" value={userName} onChange={(e) => setUserName(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                <input {...register("userName", { required: true })} type="text" name="userName" id="userName" className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                {errors.userName && <span>This field is required</span>}
             </div>
             <div>
                 <label className="block text-gray-700 font-semibold mb-2">Email:</label>
-                <input type="email" name="userEmail" id="userEmail" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                <input {...register("userEmail", { required: true })} type="email" name="userEmail" id="userEmail" className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                {errors.userEmail && <span>This field is required</span>}
             </div>
             <div>
                 <label className="block text-gray-700 font-semibold mb-2">Password:</label>
-                <input type="password" name="userPassword" id="userPassword" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                <input {...register("userPassword", { required: true })} type="password" name="userPassword" id="userPassword" className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+                {errors.userPassword && <span>This field is required</span>}
             </div>
             <button type="submit" className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4">Sign up</button>
         </form>
