aa42f1c8efc7770a0a358e5222f203102f184cb1
Kaung Si Thu <admin@Kaungs-MacBook-Pro.local>
2023-09-27 01:02:30 +0530

    week7 challenge05

--- smarter-tasks/src/pages/NotFound.tsx
+++ smarter-tasks/src/pages/NotFound.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-
+// notfound
 const NotFound: React.FC = () => {
-
-
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
             404 Not Found
