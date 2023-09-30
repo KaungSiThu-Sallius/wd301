ea38d5b4d6cbbba18fd0268a9eefdc81e1e149dd
Kaung Si Thu <admin@Kaungs-MacBook-Pro.local>
2023-09-30 23:18:08 +0530

    week 8

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -7,6 +7,12 @@ import Logout from "../pages/logout";
 import Projects from "../pages/projects"
 import Members from "../pages/members"
 import NotFound from "../pages/Notfound";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Outlet } from "react-router-dom";
+import ProjectContainer from "../pages/projects/ProjectContainer";
+import ProjectDetails from "../pages/project_details";
+import NewTask from "../pages/tasks/NewTask";
+import TaskDetailsContainer from "../pages/tasks/TaskDetailsContainer";
 
 const router = createBrowserRouter([
     { path: "/", element: <Navigate to="/account/projects" replace /> },
@@ -34,8 +40,35 @@ const router = createBrowserRouter([
             { index: true, element: <Navigate to="/account/projects" replace /> },
             {
                 path: "projects",
-                element: (<Projects />)
+                element: <ProjectContainer />,
+                children: [
+                    { index: true, element: <Projects /> },
+                    {
+                        path: ":projectID",
+                        element: <ProjectDetails />,
+                        children: [
+                            { index: true, element: <></> },
+                            {
+                                path: "tasks",
+                                children: [
+                                    { index: true, element: <Navigate to="../" /> },
+                                    {
+                                        path: "new",
+                                        element: <NewTask />,
+                                    },
+                                    {
+                                        path: ":taskID",
+                                        children: [
+                                            { index: true, element: <TaskDetailsContainer /> },
+                                        ],
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
             },
+
             {
                 path: "members",
                 element: (<Members />)
